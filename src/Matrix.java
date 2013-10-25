6109b7b5630a231cad5ff0ce74de147207b7fad7
Sean Aryana <seanaryana@gmail.com>
2013-10-25 02:50:42 -0700

    Added methods to the matrix and added some user interface

--- src/Matrix.java
+++ src/Matrix.java
@@ -7,40 +7,106 @@ import java.util.Scanner;
 public class Matrix {
 
 	private int[][] matrix;
+	private boolean selected;
 	
 	/**
 	 * Constructs a matrix
 	 * @param rows - number of rows in matrix
 	 * @param cols - number of columns in matrix
 	 */
-	public Matrix(int rows, int cols) {
-		matrix = new int[rows][cols];
+	public Matrix(int rows, int columns) {
+		if (rows == 0 || columns == 0) throw new IllegalArgumentException();
+		matrix = new int[rows][columns];
+		selected = false;
 	}
 	
+	/**
+	 * @return - number of rows in matrix
+	 */
+	public int getRows() {
+		return matrix.length;
+	}
+	
+	/**
+	 * @return - number of columns in matrix
+	 */
+	public int getColumns() {
+		return matrix[0].length;
+	}
+	
+	/**
+	 * @return - if matrix is selected
+	 */
+	public boolean getSelected() {
+		return selected;
+	}
+	
+	/**
+	 * Sets the selected state of a matrix
+	 */
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+	
+	/**
+	 * Fills matrix with user entered numbers
+	 */
 	public void fillMatrix() {
 		Scanner console = new Scanner(System.in);
-		for (int row = 0; row < matrix.length; row++) {
-			System.out.print("Number in row " + (row + 1) + ":");
+		for (int row = 0; row < this.getRows(); row++) {
+			System.out.print("Numbers in row " + (row + 1) + ": ");
 			String line = console.nextLine();
 			String[] numbers = line.split(" ");
-			if (numbers.length !=  matrix[row].length) throw new IllegalArgumentException();
-			for (int col = 0; col < matrix[row].length; col++) {
+			if (numbers.length !=  this.getColumns()) throw new IllegalArgumentException();
+			for (int col = 0; col < this.getColumns(); col++) {
 				matrix[row][col] = Integer.parseInt(numbers[col]);
 			}
 		}
 	}
 	
+	/**
+	 * Adds and stores values in matrix
+	 * @param otherMatrix - matrix being added to
+	 */
+	public void add(Matrix otherMatrix) {
+		if (otherMatrix.getRows() != this.getRows()
+				|| otherMatrix.getColumns() != this.getColumns()) {
+			throw new IllegalArgumentException();
+		}
+		
+		for (int row = 0; row < this.getRows(); row++) {
+			for (int col = 0; col < this.getColumns(); col++) {
+				matrix[row][col] += otherMatrix.matrix[row][col];
+			}
+		}
+	}
+	
+	/**
+	 * Multiplies matrix by scalar
+	 * @param scalar - number that the matrix is going to grow by
+	 */
+	public void scalarMultiplcation(int scalar) {
+		for (int row = 0; row < this.getRows(); row++) {
+			for (int col = 0; col < this.getColumns(); col++) {
+				matrix[row][col] *= scalar; 
+			}
+		}
+	}
+	
+	/**
+	 * @return - grids printed values
+	 */
 	public String toString() {
-		String matrixString = "[\n";
+		String matrixString = "\n[\n";
 		for (int row = 0; row < matrix.length; row++) {
 			for (int col = 0; col < matrix[row].length; col++) {
 				int num = matrix[row][col];
-				matrixString += num;
+				matrixString += num + " ";
 				if (num < 10) matrixString += " ";
 			}
 			matrixString += "\n";
 		}
-		matrixString += "]";
+		matrixString += "\t]";
 		return matrixString;
 	}
 	
--- src/MatrixCalculator.java
+++ src/MatrixCalculator.java
@@ -1,14 +1,93 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
- * Allows user to do operations to matracies. 
+ * Allows user to do operations to matrices. 
  * @author Sean
  *
  */
 public class MatrixCalculator {
 
+	public static Matrix[] matrices = new Matrix[2];
+	
 	public static void main(String[] args) {
-		Matrix matrixOne = new Matrix(2, 2);
+		Scanner console = new Scanner(System.in);
+		ArrayList<String> commands = getCommands();
+		
+		System.out.print("Dimensions for first matrix? (space seperated) ");
+		Matrix matrixOne = new Matrix(console.nextInt(), console.nextInt());
 		matrixOne.fillMatrix();
-		System.out.println(matrixOne.toString());
+		
+		System.out.print("Dimensions for the second matrix? (space seperated) ");
+		Matrix matrixTwo = new Matrix(console.nextInt(), console.nextInt());
+		matrixTwo.fillMatrix();
+		
+		matrices[0] = matrixOne;
+		matrices[1] = matrixTwo;
+		
+		System.out.println("Select Matrix (1 or 2)");
+		
+		if (console.nextInt() == 1) {
+			matrixOne.setSelected(true);
+		} else {
+			matrixTwo.setSelected(true);
+		}
+		
+		printCommands(commands);
+		String request = console.next().toLowerCase();
+		
+		Matrix selected = getSelectedMatrix();
+		while (!request.equals("q")) {
+			if (request.equals("fill")) {
+				selected.fillMatrix();
+			} else if (request.equals("add")) {
+				System.out.print("What matrix do you want to add to the selected matrix? (e.g. 1, 2) ");
+				selected.add(matrices[console.nextInt() - 1]);
+			} else if (request.equals("sm")) {
+				System.out.print("By what scalar? ");
+				selected.scalarMultiplcation(console.nextInt());
+			} else if (request.equals("rows")) {
+				System.out.println(selected.getRows());
+			} else if (request.equals("cols")) {
+				System.out.println(selected.getColumns());
+			} else if (request.equals("cs")) {
+				System.out.println(selected.getRows());
+			} else if (request.equals("print")) {
+				System.out.println(selected.toString());
+			}
+			System.out.println();
+			printCommands(commands);
+			request = console.next();
+		}
+	}
+	
+	private static ArrayList<String> getCommands() {
+		ArrayList<String> commands = new ArrayList<String>();
+		commands.add("Q: Quit");
+		commands.add("FILL: Fill Matrix");
+		commands.add("ADD: Add Matrix");
+		commands.add("SM: Scalar Multiply");
+		commands.add("ROWS: Get Rows");
+		commands.add("COLS: Get Columns");
+		commands.add("CS: Change Selected");
+		commands.add("PRINT: Print Matrix");
+		commands.add("What would you like to do? ");
+		return commands;
+	}
+	
+	private static void printCommands(ArrayList<String> commands) {
+		for (String command : commands) {
+			System.out.println(command);
+		}
+	}
+	
+	private static Matrix getSelectedMatrix() {
+		for (Matrix m : matrices) {
+			if (m.getSelected()) {
+				return m;
+			}
+		}
+		return null;
 	}
 
 }
